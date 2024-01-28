9b9ad8f499889d7baea972b43c9bfbf4c9e5a6bb
fedmarti <fedmarti@student.42firenze.it>
2024-01-28 23:04:59 +0100

    norminette

--- includes/keys.h
+++ includes/keys.h
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fedmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 23:03:47 by fedmarti          #+#    #+#             */
+/*   Updated: 2024/01/28 23:04:26 by fedmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef KEYS_H
-#define KEYS_H
+# define KEYS_H
 
-#define ESC 65307
-#define UP 65362
-#define DOWN 65364
-#define LEFT 65361
-#define RIGHT 65363
-#define SPACE 32
-#define SHIFT_LEFT_KEY 65505
-#define ALT_KEY 65513
-#define ENTER 65293
-#define AT_LEAST 10
+# define ESC 65307
+# define UP 65362
+# define DOWN 65364
+# define LEFT 65361
+# define RIGHT 65363
+# define SPACE 32
+# define SHIFT_LEFT_KEY 65505
+# define ALT_KEY 65513
+# define ENTER 65293
+# define AT_LEAST 10
 
-enum on_hold
+enum e_on_hold
 {
 	Null = 65361,
 	Shift = 65357,
