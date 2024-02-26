86396985ee78f5080c018440e2a32591c990d8b1
FunghiFannoBene <dsheng1993@gmail.com>
2024-02-26 09:33:53 +0100

    fast push vers.

--- src/get_color.c
+++ src/get_color.c
@@ -1,58 +1,73 @@
 #include "minirt.h"
 
+float get_actual_t(t_scene *scene, unsigned int i, t_cyl_utils *data, t_vec3 raydir)
+{
+	if (scene->shapes[i].type == Sphere)
+			return(hit_sphere(scene->shapes[i].shape.sphere, raydir, (t_vec3){scene->camera.x, scene->camera.y, scene->camera.z}));
+		else if (scene->shapes[i].type == Plane)
+			return(intersect_plane(scene->shapes[i].shape.plane, (t_vec3){scene->camera.x, scene->camera.y, scene->camera.z}, raydir));
+		else if (scene->shapes[i].type == Cylinder)
+			return(hit_cyl(scene->shapes[i].shape.cylinder, (t_vec3){scene->camera.x, scene->camera.y, scene->camera.z}, raydir, data));
+		return(0);
+}
+
+int point_and_type(float *tmp_closest, t_scene *scene, void **actual_shape, unsigned int i)
+{
+	tmp_closest[1] = tmp_closest[0];
+	if(scene->shapes[i].type == Plane)
+		*actual_shape = &scene->shapes[i].shape.plane;
+	else if(scene->shapes[i].type ==  Sphere)
+		*actual_shape = &scene->shapes[i].shape.sphere;
+	else if(scene->shapes[i].type == Cylinder)
+		*actual_shape = &scene->shapes[i].shape.cylinder;
+	return(scene->shapes[i].type);
+}
+
+int get_shape_light_color(int *closest_shape, void *actual_shape, t_scene* scene, t_cyl_utils *data)
+{
+	if (*closest_shape == Cylinder)
+	{
+		*closest_shape = -1;
+		return calculate_cylinder_color(*((t_cylinder *)actual_shape), data, scene);
+	}
+	else if (*closest_shape == Plane)
+	{
+		*closest_shape = -1;
+		return calculate_plane_color(*((t_plane *)actual_shape), scene);
+	}
+	else if (*closest_shape == Sphere)
+	{
+		*closest_shape = -1;
+		return calculate_sphere_color(*((t_sphere *)actual_shape), scene);
+	}
+	return 0x000000;
+}
+
+void init_color_utils(float *tmp_closest, unsigned int *i, int *closest_shape, void **actual_shape)
+{
+	tmp_closest[0] = INFINITY;
+	tmp_closest[1] = INFINITY;
+	*i = -1;
+	*closest_shape = -1;
+	*actual_shape = NULL;
+}
+
 int get_color(t_vec3 raydir, t_scene *scene)
 {
-	float closest_t = INFINITY;
-	float tmp_t = INFINITY;
-	int color = 0x000000;
-	unsigned int i = 0;
+	float tmp_closest[2];
+	int color;
+	unsigned int i; 
 	int 	closest_shape;
-	void *actual_shape;
+	void *actual_shape; 
 	t_cyl_utils data;
-
-	while (i < scene->shape_n)
+	
+	init_color_utils(tmp_closest, &i, &closest_shape, &actual_shape);
+	while (++i < scene->shape_n)
 	{
-		if (scene->shapes[i].type == Sphere)
-			tmp_t = hit_sphere(scene->shapes[i].shape.sphere, raydir, (t_vec3){scene->camera.x, scene->camera.y, scene->camera.z});
-		else if (scene->shapes[i].type == Plane)
-			tmp_t = intersect_plane(scene->shapes[i].shape.plane, (t_vec3){scene->camera.x, scene->camera.y, scene->camera.z}, raydir);
-		else if (scene->shapes[i].type == Cylinder)
-			tmp_t = hit_cyl(scene->shapes[i].shape.cylinder, (t_vec3){scene->camera.x, scene->camera.y, scene->camera.z}, raydir, &data);
-		if (tmp_t > 0 && tmp_t < closest_t)
-		{
-			closest_t = tmp_t;
-			if(scene->shapes[i].type == Plane)
-			{
-				actual_shape = (void *)&scene->shapes[i].shape.plane;
-				closest_shape = Plane;
-			}
-			else if(scene->shapes[i].type ==  Sphere)
-			{
-				actual_shape = (void *)&scene->shapes[i].shape.sphere;
-				closest_shape = Sphere;
-			}
-			else if(scene->shapes[i].type == Cylinder)
-			{
-				actual_shape = (void *)&scene->shapes[i].shape.cylinder;
-				closest_shape = Cylinder;
-			}
-		}
-		i++;
+		tmp_closest[0] = get_actual_t(scene, i, &data, raydir);
+		if (tmp_closest[0] > 0 && tmp_closest[0] < tmp_closest[1])
+			closest_shape = point_and_type(tmp_closest, scene, &actual_shape, i);
 	}
-	if(closest_shape == Cylinder)
-	{
-		color = calculate_cylinder_color(*((t_cylinder *)actual_shape), &data, scene);
-		closest_shape = -1;
-	}
-	else if(closest_shape == Plane)
-	{
-		color = calculate_plane_color(*((t_plane *)actual_shape), scene);
-		closest_shape = -1;
-	}
-	else if(closest_shape == Sphere)
-	{
-		color = calculate_sphere_color(*((t_sphere *)actual_shape), scene);
-		closest_shape = -1;
-	}
+	color = get_shape_light_color(&closest_shape, actual_shape, scene, &data);
 	return color;
 }
--- src/mouse.c
+++ src/mouse.c
@@ -6,6 +6,7 @@ int on_click_event(int button, int x, int y, t_mouse *info)
 	{
 		info->mouse_x = (float)x;
 		info->mouse_y = (float)y;
+		// selected_obj = ray_hit(x, y, scene);
     }
 	return 0;
 }
