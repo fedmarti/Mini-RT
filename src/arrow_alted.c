35e0e0c0adf5905cd1cfa1f49fcd1f3ffd371e59
FunghiFannoBene <dsheng1993@gmail.com>
2024-03-11 21:37:31 +0100

    fast push vers.

--- src/arrow_alted.c
+++ src/arrow_alted.c
@@ -1,47 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arrow_alted.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shhuang <dsheng1993@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 21:26:22 by shhuang           #+#    #+#             */
+/*   Updated: 2024/03/11 21:27:38 by shhuang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
 #define ROTATE_SPEED 0.005f
 
-void alt_left_on(void *general)
+void	alt_left_on(void *general)
 {
-	t_general *g;
+	t_general	*g;
 
 	g = (t_general *)general;
-	if(g->selected == NULL || g->selected_type == Sphere)
-		g->scene->camera.dir_y+= ROTATE_SPEED;
+	if (g->selected == NULL || g->selected_type == Sphere)
+		g->scene->camera.dir_y += ROTATE_SPEED;
 	else
 		*((float *)g->selected + 4) += -ROTATE_SPEED;
 }
 
-void alt_up_on(void *general)
+void	alt_up_on(void *general)
 {
-	t_general *g;
+	t_general	*g;
 
 	g = (t_general *)general;
-
-	if(g->selected == NULL || g->selected_type == Sphere)
-		g->scene->camera.dir_x-= ROTATE_SPEED;
+	if (g->selected == NULL || g->selected_type == Sphere)
+		g->scene->camera.dir_x -= ROTATE_SPEED;
 	else
 		*((float *)g->selected + 5) += ROTATE_SPEED;
 }
-void alt_right_on(void *general)
+
+void	alt_right_on(void *general)
 {
-	t_general *g;
+	t_general	*g;
 
 	g = (t_general *)general;
-	
-	if(g->selected == NULL || g->selected_type == Sphere)
-		g->scene->camera.dir_y-= ROTATE_SPEED;
+	if (g->selected == NULL || g->selected_type == Sphere)
+		g->scene->camera.dir_y -= ROTATE_SPEED;
 	else
 		*((float *)g->selected + 4) += ROTATE_SPEED;
 }
-void alt_down_on(void *general)
+
+void	alt_down_on(void *general)
 {
-	t_general *g;
+	t_general	*g;
 
 	g = (t_general *)general;
-	if(g->selected == NULL || g->selected_type == Sphere)
-		g->scene->camera.dir_x+= ROTATE_SPEED;
+	if (g->selected == NULL || g->selected_type == Sphere)
+		g->scene->camera.dir_x += ROTATE_SPEED;
 	else
 		*((float *)g->selected + 5) += -ROTATE_SPEED;
 }
--- src/arrow_shifted.c
+++ src/arrow_shifted.c
@@ -1,63 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arrow_shifted.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shhuang <dsheng1993@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 21:30:37 by shhuang           #+#    #+#             */
+/*   Updated: 2024/03/11 21:33:14 by shhuang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-void shift_left_on(void *general)
+void	shift_left_on(void *general)
 {
-	t_general *g = general;
+	t_general	*g;
+	int			diameter_point_pos;
 
-	int diameter_point_pos;
-
+	g = general;
 	diameter_point_pos = 3;
-	if(g->selected == NULL || g->selected_type == Plane)
+	if (g->selected == NULL || g->selected_type == Plane)
 		g->scene->camera.x--;
 	else
 	{
-		if(g->selected_type == Cylinder)
-			diameter_point_pos+=3;
+		if (g->selected_type == Cylinder)
+			diameter_point_pos += 3;
 		*((float *)g->selected + diameter_point_pos) -= 1;
-		if(*((float *)g->selected + diameter_point_pos) < 0)
+		if (*((float *)g->selected + diameter_point_pos) < 0)
 			*((float *)g->selected + diameter_point_pos) = 0.01;
 	}
 }
-void shift_up_on(void *general)
+
+void	shift_up_on(void *general)
 {
-	t_general *g = general;
-	
-	if(g->selected == NULL || g->selected_type == Plane)
+	t_general	*g;
+
+	g = general;
+	if (g->selected == NULL || g->selected_type == Plane)
 		g->scene->camera.y++;
-	else if(g->selected_type == Cylinder)
+	else if (g->selected_type == Cylinder)
 	{
 		*((float *)g->selected + 7) += 1;
-		if(*((float *)g->selected + 7) < 0)
+		if (*((float *)g->selected + 7) < 0)
 			*((float *)g->selected + 7) = 0.01;
 	}
 }
-void shift_right_on(void *general)
-{
-	t_general *g = general;
-	int diameter_point_pos;
 
+void	shift_right_on(void *general)
+{
+	t_general	*g;
+	int			diameter_point_pos;
+
+	g = general;
 	diameter_point_pos = 3;
-	if(g->selected == NULL || g->selected_type == Plane)
+	if (g->selected == NULL || g->selected_type == Plane)
 		g->scene->camera.x++;
 	else
 	{
-		if(g->selected_type == Cylinder)
-			diameter_point_pos+=3;
+		if (g->selected_type == Cylinder)
+			diameter_point_pos += 3;
 		*((float *)g->selected + diameter_point_pos) += 1;
-		if(*((float *)g->selected + diameter_point_pos) < 0)
+		if (*((float *)g->selected + diameter_point_pos) < 0)
 			*((float *)g->selected + diameter_point_pos) = 0.01;
 	}
 }
-void shift_down_on(void *general)
+
+void	shift_down_on(void *general)
 {
-	t_general *g = general;
-	
-	if(g->selected == NULL || g->selected_type == Plane)
+	t_general	*g;
+
+	g = general;
+	if (g->selected == NULL || g->selected_type == Plane)
 		g->scene->camera.y--;
-	else if(g->selected_type == Cylinder)
+	else if (g->selected_type == Cylinder)
 	{
 		*((float *)g->selected + 7) += 0;
-		if(*((float *)g->selected + 7) < 0)
+		if (*((float *)g->selected + 7) < 0)
 			*((float *)g->selected + 7) = 0.01;
 	}
 }
