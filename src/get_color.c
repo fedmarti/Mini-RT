b7633283bd918851b32271a524d65650e24e58b9
FunghiFannoBene <dsheng1993@gmail.com>
2024-03-11 22:06:08 +0100

    fast push vers.

--- src/get_color.c
+++ src/get_color.c
@@ -1,22 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_color.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shhuang <dsheng1993@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 21:59:38 by shhuang           #+#    #+#             */
+/*   Updated: 2024/03/11 22:05:09 by shhuang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-t_hit hit_shape(t_shape	*obj, t_camera *camera, t_cyl_utils *data, t_vec3 *raydir)
+t_hit	hit_shape(t_shape	*obj, t_camera *camera, t_cyl_utils *data,
+			t_vec3 *raydir)
 {
 	t_hit	rayhit;
-	
+
 	rayhit = (t_hit){0};
 	if (obj->type == Sphere)
-	{
-		rayhit.t = hit_sphere(&obj->shape.sphere, (t_vec3){camera->x, camera->y, camera->z}, raydir);
-	}
+		rayhit.t = hit_sphere(&obj->shape.sphere,
+				(t_vec3){camera->x, camera->y, camera->z}, raydir);
 	else if (obj->type == Plane)
-	{
-		rayhit.t = hit_plane(&obj->shape.plane, (t_vec3){camera->x, camera->y, camera->z}, raydir);
-	}
+		rayhit.t = hit_plane(&obj->shape.plane,
+				(t_vec3){camera->x, camera->y, camera->z}, raydir);
 	else if (obj->type == Cylinder)
-	{
-		rayhit.t = hit_cyl(&obj->shape.cylinder, (t_vec3){camera->x, camera->y, camera->z}, raydir, data);
-	}
+		rayhit.t = hit_cyl(&obj->shape.cylinder,
+				(t_vec3){camera->x, camera->y, camera->z}, raydir, data);
 	if (rayhit.t > 0 && rayhit.t < RAY_LENGTH)
 	{
 		rayhit.target = &obj->shape;
@@ -24,22 +34,24 @@ t_hit hit_shape(t_shape	*obj, t_camera *camera, t_cyl_utils *data, t_vec3 *raydi
 	}
 	else
 		rayhit.type = Void_shape;
-	return(rayhit);
+	return (rayhit);
 }
 
-int point_and_type(float *tmp_closest, t_scene *scene, void **actual_shape, unsigned int i)
+int	point_and_type(float *tmp_closest, t_scene *scene, void **actual_shape,
+		unsigned int i)
 {
 	tmp_closest[1] = tmp_closest[0];
-	if(scene->shapes[i].type == Plane)
+	if (scene->shapes[i].type == Plane)
 		*actual_shape = &scene->shapes[i].shape.plane;
-	else if(scene->shapes[i].type ==  Sphere)
+	else if (scene->shapes[i].type == Sphere)
 		*actual_shape = &scene->shapes[i].shape.sphere;
-	else if(scene->shapes[i].type == Cylinder)
+	else if (scene->shapes[i].type == Cylinder)
 		*actual_shape = &scene->shapes[i].shape.cylinder;
-	return(scene->shapes[i].type);
+	return (scene->shapes[i].type);
 }
 
-void init_color_utils(float *tmp_closest, unsigned int *i, int *closest_shape, void **actual_shape)
+void	init_color_utils(float *tmp_closest, unsigned int *i,
+			int *closest_shape, void **actual_shape)
 {
 	tmp_closest[0] = INFINITY;
 	tmp_closest[1] = INFINITY;
@@ -48,18 +60,20 @@ void init_color_utils(float *tmp_closest, unsigned int *i, int *closest_shape, v
 	*actual_shape = NULL;
 }
 
-int ambient_color(unsigned int color, float ratio)
+int	ambient_color(unsigned int color, float ratio)
 {
-	return ((unsigned int)(((color >> 16) & 0xFF) * ratio) << 16) | ((unsigned int)(((color >> 8) & 0xFF) * ratio) << 8) | ((unsigned int)((color & 0xFF) * ratio));
+	return (((unsigned int)(((color >> 16) & 0xFF) * ratio) << 16)
+	| ((unsigned int)(((color >> 8) & 0xFF) * ratio) << 8)
+	| ((unsigned int)((color & 0xFF) * ratio)));
 }
 
-int get_color(t_vec3 raydir, t_scene *scene)
+int	get_color(t_vec3 raydir, t_scene *scene)
 {
-	unsigned int i; 
-	t_cyl_utils data;
-	t_hit	rayhit;
-	t_hit	temp_hit;
-	
+	unsigned int	i;
+	t_cyl_utils		data;
+	t_hit			rayhit;
+	t_hit			temp_hit;
+
 	rayhit = (t_hit){INFINITY, NULL, Void_shape};
 	i = -1;
 	while (++i < scene->shape_n)
@@ -69,10 +83,10 @@ int get_color(t_vec3 raydir, t_scene *scene)
 			rayhit = temp_hit;
 	}
 	if (rayhit.type == Cylinder)
-		return(calculate_cylinder_c(rayhit.target, &data, scene, raydir));
+		return (calculate_cylinder_c(rayhit.target, &data, scene, raydir));
 	else if (rayhit.type == Plane)
-		return(calculate_plane_color(rayhit.target, scene, raydir, rayhit.t));
+		return (calculate_plane_color(rayhit.target, scene, raydir, rayhit.t));
 	else if (rayhit.type == Sphere)
-		return(calculate_sphere_color(rayhit.target, scene, raydir, rayhit.t));
-	return(ambient_color(scene->ambient.color, scene->ambient.ratio));
+		return (calculate_sphere_color(rayhit.target, scene, raydir, rayhit.t));
+	return (ambient_color(scene->ambient.color, scene->ambient.ratio));
 }
