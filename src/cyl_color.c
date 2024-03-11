c4285d242311f0444675bff4b136cd0101eb2aee
FunghiFannoBene <dsheng1993@gmail.com>
2024-03-11 21:49:21 +0100

    fast push vers.

--- src/cyl_color.c
+++ src/cyl_color.c
@@ -1,36 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cyl_color.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shhuang <dsheng1993@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 21:40:44 by shhuang           #+#    #+#             */
+/*   Updated: 2024/03/11 21:49:12 by shhuang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-// static int calculate_cyl_extern(int color)
-// {
-// 	return(color);
-// }
-
-// static int calculate_cyl_intern(int color)
-// {
-// 	return(color);
-// }
-
-// static int calculate_cylinder_base(int color)
-// {
-// 	return(color);
-// }
-
-int calculate_cylinder_color(t_cylinder *cyl, t_cyl_utils *data, t_scene *scene, t_vec3 raydir)
+int	calculate_cylinder_c(t_cylinder *c, t_cyl_utils *d, t_scene *s, t_vec3 rd)
 {
-    t_vec3 hit_point;
-    t_vec3 norm;
+	t_vec3	hit_point;
+	t_vec3	norm;
 
-    if(data->flag == Inside_Surface)
-        return(cyl->color);
-    hit_point = vec3_add((t_vec3){scene->camera.x, scene->camera.y, scene->camera.z}, vec3_scale(raydir, data->min_t));
-	if(data->flag == Base)
+	if (d->flag == Inside_Surface)
+		return (c->color);
+	hit_point = vec3_add((t_vec3){s->camera.x, s->camera.y, s->camera.z},
+			vec3_scale(rd, d->min_t));
+	if (d->flag == Base)
 	{
-		if(data->base_is_top)
-			norm = vec3_normalize((t_vec3){cyl->normal_x, cyl->normal_y, cyl->normal_z});
+		if (d->base_is_top)
+			norm = vec3_normalize((t_vec3){c->normal_x,
+					c->normal_y,
+					c->normal_z});
 		else
-			norm = vec3_normalize(vec3_invert((t_vec3){cyl->normal_x, cyl->normal_y, cyl->normal_z}));
+			norm = vec3_normalize(vec3_invert((t_vec3){c->normal_x,
+						c->normal_y,
+						c->normal_z}));
 	}
-    else
-        norm = vec3_normalize(vec3_substract(hit_point, (t_vec3){cyl->x, cyl->y, cyl->z}));
-    return(loop_light(scene, hit_point, norm, cyl->color));
+	else
+		norm = vec3_normalize(vec3_substract(hit_point,
+					(t_vec3){c->x, c->y, c->z}));
+	return (loop_light(s, hit_point, norm, c->color));
 }
