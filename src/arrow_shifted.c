39582004709fb979b7942b215984e48b51b41d68
Federico Carlo Martini <fedmarti@c2r5p12.42firenze.it>
2024-04-11 16:57:48 +0200

    shitted

--- src/arrow_shifted.c
+++ src/arrow_shifted.c
@@ -6,7 +6,7 @@
 /*   By: fedmarti <fedmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 21:30:37 by shhuang           #+#    #+#             */
-/*   Updated: 2024/04/10 21:57:17 by fedmarti         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:24:25 by fedmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,61 @@
 void	shift_left_on(void *general)
 {
 	t_general	*g;
-	int			diameter_point_pos;
+	float		*size;
 
 	g = general;
-	diameter_point_pos = 3;
 	if (g->selected == NULL || g->selected->type == Plane)
 		g->scene->camera.x--;
 	else
 	{
 		if (g->selected->type == Cylinder)
-			diameter_point_pos += 3;
-		*((float *)g->selected + diameter_point_pos) -= 1;
-		if (*((float *)g->selected + diameter_point_pos) < 0)
-			*((float *)g->selected + diameter_point_pos) = 0.01;
+		{
+			size = &g->selected->shape.cylinder.diameter;
+		}
+		else if (g->selected->type == Sphere)
+			size = &g->selected->shape.sphere.diameter;
+		else
+			return ;
+		*size -= 1;
+		if (*size < 0)
+			*size = 0.01;
 	}
 }
 
 void	shift_up_on(void *general)
 {
 	t_general	*g;
+	float		*y;
 
 	g = general;
-	if (g->selected == NULL || g->selected->type == Plane)
+	if (g->selected == NULL)
 		g->scene->camera.y++;
-	else if (g->selected->type == Cylinder)
+	else
 	{
-		*((float *)g->selected + 7) += 1;
-		if (*((float *)g->selected + 7) < 0)
-			*((float *)g->selected + 7) = 0.01;
+		pointer_to_position(g->selected, NULL, &y, NULL);
+		*y += STEPS;
 	}
 }
 
 void	shift_right_on(void *general)
 {
 	t_general	*g;
-	int			diameter_point_pos;
+	float		*size;
 
 	g = general;
-	diameter_point_pos = 3;
 	if (g->selected == NULL || g->selected->type == Plane)
 		g->scene->camera.x++;
 	else
 	{
 		if (g->selected->type == Cylinder)
-			diameter_point_pos += 3;
-		*((float *)g->selected + diameter_point_pos) += 1;
-		if (*((float *)g->selected + diameter_point_pos) < 0)
-			*((float *)g->selected + diameter_point_pos) = 0.01;
+		{
+			size = &g->selected->shape.cylinder.diameter;
+		}
+		else if (g->selected->type == Sphere)
+			size = &g->selected->shape.sphere.diameter;
+		else
+			return ;
+		*size += (*size < MAX_SHAPE_SIZE);
 	}
 }
 
@@ -71,13 +79,11 @@ void	shift_down_on(void *general)
 	float		*y;
 
 	g = general;
-	if (g->selected == NULL || g->selected->type == Plane)
+	if (g->selected == NULL)
 		g->scene->camera.y--;
-	else if (g->selected->type == Cylinder)
+	else
 	{
 		pointer_to_position(g->selected, NULL, &y, NULL);
-		*y += STEPS;
-		// if (*((float *)g->selected + 7) < 0)
-			// *((float *)g->selected + 7) = 0.01;
+		*y -= STEPS;
 	}
 }
